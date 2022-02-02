2f5450b1357a59916390d718110a12ca15ab29f5
Adam <Adam@DESKTOP-4EFD29B>
2022-02-02 12:06:05 -0500

    adding 03_Operators to Fundamentals Solution/Project

---
+++ 03_Operators/Program.cs
@@ -0,0 +1,22 @@
+﻿int additionResult = 5 + 2;
+Console.WriteLine("5 + 2 = " + additionResult);
+
+// int numOne = 5;
+// int numTwo = 2;
+// int result = numOne + numTwo;
+
+int subtractionResult = 5 - 2;
+Console.WriteLine("5 - 2 = " + subtractionResult);
+
+int multiplicationResult = 5 * 2;
+Console.WriteLine("5 * 2 = " + multiplicationResult);
+
+double divisionResult = 5d / 2d;
+Console.WriteLine("5 / 2 = " + divisionResult);
+
+Console.WriteLine(10 % 2);
+Console.WriteLine(10 % 9);
+Console.WriteLine(16 % 3);
+Console.WriteLine(19 % 5);
+
+
