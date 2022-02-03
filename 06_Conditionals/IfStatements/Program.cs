cbecc21d4b28e17e06c689f423df39c69d34ad42
Adam <Adam@DESKTOP-4EFD29B>
2022-02-03 10:18:55 -0500

    adding 06_Conditionals/IfStatements to Fundamental Solution/Project

---
+++ 06_Conditionals/IfStatements/Program.cs
@@ -0,0 +1,40 @@
+﻿bool isSnowing = true;
+bool isGoingOutside = false;
+
+if(isSnowing)
+{
+Console.WriteLine("It is snowing out!");
+}
+
+if(isSnowing && isGoingOutside)
+{
+Console.WriteLine("Better where some snow boots!!!");
+}
+if(!isSnowing || !isGoingOutside)
+{
+Console.WriteLine("I do not need any boots!!");
+}
+if(isSnowing ^ isGoingOutside)
+{
+Console.WriteLine("I will only go outside once the snow stops!!!");
+}
+
+
+
+if(!isSnowing){
+    Console.WriteLine("There is no need to shovel snow!!");
+}
+else{
+    Console.WriteLine("I guess I need to shovel the snow!");
+}
+
+
+if(isSnowing){
+    Console.WriteLine("I guess I have to go clear off the wifes cars!!");
+}
+else if(!isSnowing){
+    Console.WriteLine("I dont have to clear off the cars!!");
+}
+else{
+    Console.WriteLine("I dont know what we are even talking about!!");
+}
