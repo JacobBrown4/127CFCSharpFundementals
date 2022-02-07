ec5c455c72343d5967ff865560537d6d44c7a024
jacobbrown4 <jacobbrown4@gmail.com>
2022-02-07 11:48:27 -0500

    Added loops

---
+++ 07_Loops/Foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿string instructorName = "Jacob";
+string[] instructors = new string[] {"Adam","Kate",instructorName};
+//List<string>
+
+// foreach(declaredIterator in enumerableCollection){body}
+
+foreach(string name in instructors){
+    Console.WriteLine($"Hello {name}");
+}
+                         //Strings are collections of characters, 
+                         //and can be iterated over in foreach's
+foreach(char letter in instructorName){
+    if(letter == 'c'){
+        Console.WriteLine('k');
+    }
+    else
+    Console.WriteLine(letter);
+}
---
+++ 07_Loops/While/Program.cs
@@ -0,0 +1,13 @@
+﻿// Declare a variable
+int number = 0;
+
+// Keyword  Condition
+while (number <= 100) // If condition is true; loop!
+{
+    //Loop
+    // Print the number and add 5
+    Console.WriteLine(number);
+    number = number + 5;
+}
+
+// If in an infite loop ctrl+c (cmd+c)
